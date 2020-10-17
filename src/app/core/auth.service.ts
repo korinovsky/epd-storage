d2ee17bea9e757c98a248730e7bbe9c6ba58f164
Konstantin Korinovsky <k.korinovsky@gmail.com>
2020-10-17 11:49:47 +0300

    update and deploy

--- src/app/core/auth.service.ts
+++ src/app/core/auth.service.ts
@@ -1,6 +1,5 @@
 import {Injectable} from '@angular/core';
 import {auth, User as FirebaseUser} from 'firebase/app';
-import {FirebaseAuth} from '@angular/fire';
 import {AngularFireAuth} from '@angular/fire/auth';
 import {BehaviorSubject, Observable} from 'rxjs';
 import {fromPromise} from 'rxjs/internal-compatibility';
@@ -14,11 +13,9 @@ export type AuthUser = FirebaseUser;
 export class AuthService {
     readonly user$ = new BehaviorSubject<AuthUser>(undefined);
     readonly isUserLoggedIn$ = new BehaviorSubject(false);
-    private readonly auth: FirebaseAuth;
 
-    constructor(angularFireAuth: AngularFireAuth) {
-        angularFireAuth.user.subscribe(user => this.user = user);
-        this.auth = angularFireAuth.auth;
+    constructor(private fireAuth: AngularFireAuth) {
+        fireAuth.user.subscribe(user => this.user = user);
         this.user$.subscribe(user => this.isUserLoggedIn$.next(!!user));
     }
 
@@ -31,13 +28,13 @@ export class AuthService {
     }
 
     login$(): Observable<AuthUser> {
-        return fromPromise(this.auth.signInWithPopup(new auth.GoogleAuthProvider())).pipe(
+        return fromPromise(this.fireAuth.signInWithPopup(new auth.GoogleAuthProvider())).pipe(
             map(user => this.user = user.user)
         );
     }
 
     logout$(): Observable<void> {
-        return fromPromise(this.auth.signOut()).pipe(
+        return fromPromise(this.fireAuth.signOut()).pipe(
             tap(() => this.user = null)
         );
     }
